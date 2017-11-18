f0fe203ab78b8caaac24e319cceeff964ba88047
iLuisYM <luis.yauri1@unmsm.edu.pe>
2017-11-18 11:33:48 -0500

    Luis

--- src/test/Administrador.java
+++ src/test/Administrador.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package test;
-
+//luis
 import controlador.administrador.AdministradorController;
 import factory.ConnectionDb;
 import factory.FactoryConnectionDb;
