7891d768449699e2fa477b71b15c3e81aa42ccf3
AlissonArroyo <alisson.arroyo@unmsm.edu.pe>
2017-11-21 18:25:00 -0500

    -_-'

--- src/test/MainMedicoEspecialista.java
+++ src/test/MainMedicoEspecialista.java
@@ -19,15 +19,15 @@ public class MainMedicoEspecialista {
     public static void main(String[] args) {
         // TODO code application logic here
          MedicoEspecialista medicoEspecialista = new MedicoEspecialista("Cardiólogo","Colegiatura","1234", "contraseña", null, 21, "Carlos", "Sanchez", 9098, true, 32, 9563212, 6411452, "carlos@gmail.com", 0);
-//
-         Paciente p1 = new Paciente(0, "Luis", "Luis", 1111, true, 21, 98766, 64564, "", 2, "A", "No hay");
-         Paciente p2 = new Paciente(0, "Yauri", "Yauri", 2222, true, 21, 98766, 64564, "", 2, "A", "No hay");
-         Paciente p3 = new Paciente(0, "Miriam", "Miriam", 3333, true, 21, 98766, 64564, "", 2, "A", "No hay");
-         Paciente p4 = new Paciente(0, "Mizu", "Mizu", 4444, true, 21, 98766, 64564, "", 2, "A", "No hay");
-         Paciente p5 = new Paciente(0, "Luis", "Luis", 1111, true, 21, 98766, 64564, "", 2, "A", "No hay");
-         Paciente p6 = new Paciente(0, "LuisRoberto", "LuisRoberto", 6666, true, 21, 98766, 64564, "", 2, "A", "No hay");
-         Paciente p7 = new Paciente(0, "LuisJose", "LuisJose", 5555, true, 21, 98766, 64564, "", 2, "A", "No hay");
-         
+////
+//         Paciente p1 = new Paciente(0, "Luis", "Luis", 1111, true, 21, 98766, 64564, "", 2, "A", "No hay");
+//         Paciente p2 = new Paciente(0, "Yauri", "Yauri", 2222, true, 21, 98766, 64564, "", 2, "A", "No hay");
+//         Paciente p3 = new Paciente(0, "Miriam", "Miriam", 3333, true, 21, 98766, 64564, "", 2, "A", "No hay");
+//         Paciente p4 = new Paciente(0, "Mizu", "Mizu", 4444, true, 21, 98766, 64564, "", 2, "A", "No hay");
+//         Paciente p5 = new Paciente(0, "Luis", "Luis", 1111, true, 21, 98766, 64564, "", 2, "A", "No hay");
+//         Paciente p6 = new Paciente(0, "LuisRoberto", "LuisRoberto", 6666, true, 21, 98766, 64564, "", 2, "A", "No hay");
+//         Paciente p7 = new Paciente(0, "LuisJose", "LuisJose", 5555, true, 21, 98766, 64564, "", 2, "A", "No hay");
+//         
         
          
          ListaDoble<Visita> listav1 = new ListaDoble<Visita>();
@@ -52,24 +52,24 @@ public class MainMedicoEspecialista {
          
          
          
-         p1.getHistorial().setListaVisitas(listav1);
-         
-         p2.getHistorial().setListaVisitas(listav2);
-         p3.getHistorial().setListaVisitas(listav3);
-         p4.getHistorial().setListaVisitas(listav4);
+//         p1.getHistorial().setListaVisitas(listav1);
 //         
+//         p2.getHistorial().setListaVisitas(listav2);
+//         p3.getHistorial().setListaVisitas(listav3);
+//         p4.getHistorial().setListaVisitas(listav4);
+////         
      
          
         LocalDate l = LocalDate.now();
         System.out.println("FECHA: " + l.toString());
           
-        medicoEspecialista.agregarPaciente(p1);
-        medicoEspecialista.agregarPaciente(p2);
-        medicoEspecialista.agregarPaciente(p3);
-        medicoEspecialista.agregarPaciente(p4);
-        medicoEspecialista.agregarPaciente(p5);
-        medicoEspecialista.agregarPaciente(p6);
-        medicoEspecialista.agregarPaciente(p7);
+//        medicoEspecialista.agregarPaciente(p1);
+//        medicoEspecialista.agregarPaciente(p2);
+//        medicoEspecialista.agregarPaciente(p3);
+//        medicoEspecialista.agregarPaciente(p4);
+//        medicoEspecialista.agregarPaciente(p5);
+//        medicoEspecialista.agregarPaciente(p6);
+//        medicoEspecialista.agregarPaciente(p7);
         
         
         FrameMedicoEspecialista fm = new FrameMedicoEspecialista(medicoEspecialista);
--- src/test/mainContaTest.java
+++ src/test/mainContaTest.java
@@ -17,14 +17,14 @@ public class mainContaTest {
         // TODO code application logic here
          Cajero caja = new Cajero("1234", "contraseña", null, 21, "Carlos", "Sanchez", 9098, true, 32, 9563212, 6411452, "carlos@gmail.com", 0);
          MedicoTriaje triaje = new MedicoTriaje("triaje", "5694", "contraseña",null, 13, "Mónica","Blanco", 77777777, false, 42, 555555,999999, "mb@ail.com", 0);
-           Paciente p1 = new Paciente(0, "Luis", "Luis", 1111, true, 21, 98766, 64564, "", 2, "A", "No hay");
-         Paciente p2 = new Paciente(0, "Yauri", "Yauri", 2222, true, 21, 98766, 64564, "", 2, "A", "No hay");
-         Paciente p3 = new Paciente(0, "Miriam", "Miriam", 3333, true, 21, 98766, 64564, "", 2, "A", "No hay");
-         Paciente p4 = new Paciente(0, "Mizu", "Mizu", 4444, true, 21, 98766, 64564, "", 2, "A", "No hay");
-         Paciente p5 = new Paciente(0, "Luis", "Luis", 1111, true, 21, 98766, 64564, "", 2, "A", "No hay");
-         Paciente p6 = new Paciente(0, "LuisRoberto", "LuisRoberto", 6666, true, 21, 98766, 64564, "", 2, "A", "No hay");
-         Paciente p7 = new Paciente(0, "LuisJose", "LuisJose", 5555, true, 21, 98766, 64564, "", 2, "A", "No hay");
-         
+//           Paciente p1 = new Paciente(0, "Luis", "Luis", 1111, true, 21, 98766, 64564, "", 2, "A", "No hay");
+//         Paciente p2 = new Paciente(0, "Yauri", "Yauri", 2222, true, 21, 98766, 64564, "", 2, "A", "No hay");
+//         Paciente p3 = new Paciente(0, "Miriam", "Miriam", 3333, true, 21, 98766, 64564, "", 2, "A", "No hay");
+//         Paciente p4 = new Paciente(0, "Mizu", "Mizu", 4444, true, 21, 98766, 64564, "", 2, "A", "No hay");
+//         Paciente p5 = new Paciente(0, "Luis", "Luis", 1111, true, 21, 98766, 64564, "", 2, "A", "No hay");
+//         Paciente p6 = new Paciente(0, "LuisRoberto", "LuisRoberto", 6666, true, 21, 98766, 64564, "", 2, "A", "No hay");
+//         Paciente p7 = new Paciente(0, "LuisJose", "LuisJose", 5555, true, 21, 98766, 64564, "", 2, "A", "No hay");
+//         
          
         Servicios s1 = new Servicios("sangre", 230F, false);
         Servicios s2 = new Servicios("glusoca", 250F, false);
@@ -36,13 +36,13 @@ public class mainContaTest {
         LocalDate l = LocalDate.now();
         System.out.println("FECHA: " + l.toString());
           
-        caja.agregarPaciente(p1);
-        caja.agregarPaciente(p2);
-        caja.agregarPaciente(p3);
-        caja.agregarPaciente(p4);
-        caja.agregarPaciente(p5);
-        caja.agregarPaciente(p6);
-        caja.agregarPaciente(p7);
+//        caja.agregarPaciente(p1);
+//        caja.agregarPaciente(p2);
+//        caja.agregarPaciente(p3);
+//        caja.agregarPaciente(p4);
+//        caja.agregarPaciente(p5);
+//        caja.agregarPaciente(p6);
+//        caja.agregarPaciente(p7);
         
         caja.agregarServicios(s1);
         caja.agregarServicios(s2);
